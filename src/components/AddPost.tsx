550b0f5877f37315f7a59e6c78e3c6b39abc1601
Gamal Ahmed <gamalahmed3265@gmail.com>
2024-08-24 17:09:59 +0300

    Finish Add New Posts Component

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,41 @@
+import Image from "next/image"
 
 const AddPost = () => {
   return (
-    <div>AddPost</div>
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/* Avaterr */}
+      <div className="">
+        <Image src="https://media.istockphoto.com/id/1278518477/photo/the-camel-caravan-is-in-front-of-the-egyptian-pyramids.jpg?s=612x612&w=is&k=20&c=m1dVzbl9PLy0n7lANdnNUwBSPlnwQIWlbzAY_AIFtm0=" className="w-12 h-12 object-cover rounded-full" width={48} height={48} alt=""/>
+      </div>
+      {/* Add Post */}
+      <div className="flex-1">
+        {/* Text Input */}
+        <div className="flex gap-4">
+          <textarea name="" placeholder="What`s Your Mind" className="bg-slate-100 rounded-lg p-2 flex-1" id=""></textarea>
+          <Image src="/emoji.png" className="w-5 h-5 cursor-pointer self-end" width={20} height={20} alt=""/>
+
+        </div>
+        {/* Post Options */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addimage.png" alt="" width={20} height={20}/>
+            Image
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" alt="" width={20} height={20}/>
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/events.png" alt="" width={20} height={20}/>
+            Events
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="" width={20} height={20}/>
+            Poll
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
