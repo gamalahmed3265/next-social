9076282b4f8b7012af9c57a8648dc5718d7e0ac2
Gamal Ahmed <gamalahmed3265@gmail.com>
2024-09-28 19:53:40 +0300

    ADD config prisma , mysql ,cloudplane

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -5,7 +5,7 @@ const ProfileCard = () => {
         <div className="p-2 rounded-lg bg-white shadow-md text-sm flex flex-col gap-6">
             <div className="h-20 relative">
                 <Image src="https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="rounded-md" fill />
-                <Image src="https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="w-12 h-12 rounded-full absolute left-0 right-0 m-auto bottom-6 ring-1 ring-white z-10" width={48} height={48} />
+                <Image src="https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="w-12 h-12 rounded-full absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10" width={48} height={48} />
             </div>
             <div className="flex flex-col gap-2 h-24 items-center">
                 <span className="font-semibold">Andrea Piacquadio </span>
