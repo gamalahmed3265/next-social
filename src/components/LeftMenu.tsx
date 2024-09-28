1fb9944fcd0be59711df264e27188161379094de
Gamal Ahmed <gamalahmed3265@gmail.com>
2024-09-28 13:15:58 +0300

    Finish design left bar

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,7 +1,57 @@
+import Image from "next/image"
+import ProfileCard from "./ProfileCard"
+import Link from "next/link"
+import Ad from "./Ad"
 
-const LeftMenu = () => {
+const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
   return (
-    <div>LeftMenu</div>
+    <div className="flex flex-col gap-6">
+      {type === "home" && <ProfileCard />}
+      <div className="p-2 rounded-lg bg-white shadow-md text-sm text-gray-500 flex flex-col gap-2">
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/posts.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>MyPosts</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/activity.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>Activity</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/market.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>Marketplace</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/events.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>Events</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/albums.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>Albums</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/videos.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>Videos</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/news.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>News</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/courses.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>Courese</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/lists.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>Lists</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100 font-medium">
+            <Image src="/settings.png" alt="" className="w-5 h-5" width={20} height={20} />
+            <span>Settings</span>
+          </Link>
+
+      </div>
+      <Ad size="sm"/>
+    </div>
   )
 }
 
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const ProfileCard = () => {
+    return (
+        <div className="p-2 rounded-lg bg-white shadow-md text-sm flex flex-col gap-6">
+            <div className="h-20 relative">
+                <Image src="https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="rounded-md" fill />
+                <Image src="https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="w-12 h-12 rounded-full absolute left-0 right-0 m-auto bottom-6 ring-1 ring-white z-10" width={48} height={48} />
+            </div>
+            <div className="flex flex-col gap-2 h-24 items-center">
+                <span className="font-semibold">Andrea Piacquadio </span>
+                <div className="flex items-center gap-4">
+                    <div className="flex">
+                        <Image src="/courses.png" alt="" className="w-3 h-3 rounded-full object-cover " width={12} height={12} />
+                        <Image src="/courses.png" alt="" className="w-3 h-3 rounded-full object-cover " width={12} height={12} />
+                        <Image src="/courses.png" alt="" className="w-3 h-3 rounded-full object-cover " width={12} height={12} />
+                    </div>
+                    <span className="text-xs text-gray-500">500 Followers</span>
+                </div>
+                <button  className="bg-blue-500 text-white text-xs rounded-md w-fit p-2">
+                    MyProfile
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileCard
